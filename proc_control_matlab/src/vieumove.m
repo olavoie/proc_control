40436e820b554d776c68782dabf887ef0fdd3993
Alexandre Desgagné <alexandre1998@live.ca>
2020-06-27 19:55:06 -0400

    Added Coriolis Matrix.

--- proc_control_matlab/src/vieumove.m
+++ proc_control_matlab/src/vieumove.m
@@ -1,10 +1,11 @@
 clc;
-
+v = [0.5 0.5 1 0 0 0.1];
 x=diag(repmat(2,1,3))-1
 
 C= ConfigAUV8
 
  m = C.mass;
+ i = C.I;
             xg = C.RG(1);
             yg = C.RG(2);
             zg = C.RG(3);
@@ -32,3 +33,22 @@ C= ConfigAUV8
             
             % Calcul de la matrice des masses et inerties.
             M = MRB + MA
+            
+ % Définition des matrices 3x3 pour former la matrice de
+            % Coriolis (corps rigide).
+            crb1 = zeros(3,3);
+            crb23 = [0, m*v(3), -m*v(2); ...
+                     -m*v(3), 0, m*v(1); ...
+                     m*v(2), -m*v(1), 0];
+            crb4 = [0, i(3,3)*v(6), -i(2,2)*v(5); ...
+                    -i(3,3)*v(6), 0, i(1,1)*v(4); ...
+                    i(2,2)*v(5), -i(1,1)*v(4), 0];
+            % Matrice de Coriolis(Corps rigide)
+            CRB = [crb1, crb23; ...
+                   crb23, crb4];
+            
+            % Matrice de Coriolis (masse ajoutée)
+            CA = zeros(6,6);
+            
+            % Matrice de Coriolis
+            C = CRB + CA
