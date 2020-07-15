960a9b59ea944fa534b2bca5c775de4be2cb322a
Lamarre <Lamarre@ALEX-PC>
2020-07-15 12:25:41 -0400

    fix trusters motor

--- proc_control_matlab/src/Modele Dinamique/QuadrotorReferenceTrajectory.m
+++ proc_control_matlab/src/Modele Dinamique/QuadrotorReferenceTrajectory.m
@@ -6,21 +6,24 @@ function [ xdesired ] = QuadrotorReferenceTrajectory( t )
 
 %#codegen
 
-if t<=8
-
-x = 0*t; %6*sin(t/3);
+%if t<=8
+% 
+% x = 0*t; %6*sin(t/3);
+% y = 0*t; %-6*sin(t/3).*cos(t/3);
+% z = -t/5; %6*cos(t/3);
+% elseif t<12
+% x = 0*t; %6*sin(t/3);
+% y = 0*t; %-6*sin(t/3).*cos(t/3);
+% z = -1.6+0*t; %6*cos(t/3); 
+% else
+% x = (t-12); %6*sin(t/3);
+% y = 0*t; %-6*sin(t/3).*cos(t/3);
+% z = 0*t-1.6; %6*cos(t/3);
+% end
+x = t/3; %6*sin(t/3);
 y = 0*t; %-6*sin(t/3).*cos(t/3);
-z = -t/5; %6*cos(t/3);
-elseif t<12
-x = 0*t; %6*sin(t/3);
-y = 0*t; %-6*sin(t/3).*cos(t/3);
-z = -1.6+0*t; %6*cos(t/3); 
-else
-x = (t-12); %6*sin(t/3);
-y = 0*t; %-6*sin(t/3).*cos(t/3);
-z = 0*t-1.6; %6*cos(t/3);
-end
-phi = zeros(1,length(t));
+z = 0*t; %6*cos(t/3);
+phi = t/3;zeros(1,length(t));
 theta = zeros(1,length(t));
 psi = zeros(1,length(t));
 xdot = zeros(1,length(t));
