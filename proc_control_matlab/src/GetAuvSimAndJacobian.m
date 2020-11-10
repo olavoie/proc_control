99a7fdea666dedf8cb0280fd2bc8e7cc451c2deb
Lamarre <Lamarre@ALEX-PC>
2020-11-09 19:34:26 -0500

    fixed alex's name

--- proc_control_matlab/src/GetAuvSimAndJacobian.m
+++ proc_control_matlab/src/GetAuvSimAndJacobian.m
@@ -2,7 +2,7 @@
 % de la dynamique du sous marin. Il génere par la suite les équations
 % d'états ainsi que la jacobienne qui seront utilisés par le controlleur.
 
-% Alexandre Lamarre, Aleandre Desgagné
+% Alexandre Lamarre, Alexandre Desgagné
 
 %clear; clc;
 %% Création des variables Symboliques
@@ -147,9 +147,9 @@
 % des masses du corp rigide.
     Mrb1 = diag(repmat(mass,1,3));
     Mrb2 = [0, mass * RG3, -mass * RG2; ...
-            -mass * RG3, 0, mass * RG1; .
-% Alexandre Lamarre, Ale..
+            -mass * RG3, 0, mass * RG1; ...
             mass * RG2, -mass * RG1, 0];
+        
     Mrb3 = [0, -mass * RG3, mass * RG2; ...
             mass * RG3, 0, -mass * RG1; ...
             -mass * RG2, mass * RG1, 0];
